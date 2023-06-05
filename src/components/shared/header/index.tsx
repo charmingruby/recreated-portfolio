ad898528ddc9d901be63597dde031920157732ab
charmingruby <mrrubycanal3105@gmail.com>
2023-06-05 20:14:05 -0300

    drawer errors fix

--- src/components/shared/header/index.tsx
+++ src/components/shared/header/index.tsx
@@ -1,9 +1,9 @@
-import { MobileHeader } from './menu-handler'
+import { Drawer } from './drawer'
 
 export function Header() {
   return (
     <div className="bg-blur fixed z-50 flex h-16 w-full justify-center border-b border-dark-light opacity-80 backdrop-blur-lg">
-      <MobileHeader />
+      <Drawer />
     </div>
   )
 }
