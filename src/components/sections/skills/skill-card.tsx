67e47af75424f4fd4e7defe86bc6d8545e51405d
charmingruby <mrrubycanal3105@gmail.com>
2023-07-25 01:46:35 -0300

    fixed dates removed

--- src/components/sections/skills/skill-card.tsx
+++ src/components/sections/skills/skill-card.tsx
@@ -1,5 +1,4 @@
 import { ISkill } from '@/data/skillsList'
-import { FilledCalendarIcon } from '@/assets/icons/usabilities'
 
 interface SkillCardProps {
   skill: ISkill
@@ -8,17 +7,9 @@ interface SkillCardProps {
 export function SkillCard({ skill }: SkillCardProps) {
   return (
     <div className="group flex w-full cursor-pointer flex-col gap-2 rounded-md border border-light-darker bg-dark-lighten px-3 py-2 text-base text-light-main transition-colors hover:border-solid hover:border-primary-main hover:bg-dark-light hover:text-primary-main">
-      <div className="flex w-full justify-between">
+      <div className="flex w-full items-center justify-between">
         <span className="font-alt text-lg">{skill.name}</span>
         <span>{skill.icon}</span>
-      </div>
-
-      <div className="flex items-center gap-2 text-light-dark">
-        <FilledCalendarIcon className="" size={12} />
-        <span className="sm:hidden">{skill.experienceTime}</span>
-        <span className="hidden sm:block">
-          {skill.experienceTime} of experience.
-        </span>
       </div>
     </div>
   )
--- src/data/skillsList.tsx
+++ src/data/skillsList.tsx
@@ -4,85 +4,60 @@ import * as Tecs from '../assets/icons/technologies'
 export interface ISkill {
   name: string
   icon: ReactNode
-  experienceTime: string
 }
 
 export const frontEndSkills: ISkill[] = [
-  {
-    icon: <Tecs.NextIcon />,
-    name: 'Next',
-    experienceTime: '1 year',
-  },
-  {
-    icon: <Tecs.ReactIcon />,
-
-    name: 'React',
-    experienceTime: '2 year',
-  },
+  { icon: <Tecs.NextIcon />, name: 'Next' },
+  { icon: <Tecs.ReactIcon />, name: 'React' },
 ]
 
 export const backEndSkills: ISkill[] = [
-  {
-    icon: <Tecs.NodeIcon />,
-
-    name: 'Node',
-    experienceTime: '1.5 year',
-  },
+  { icon: <Tecs.NodeIcon />, name: 'Node' },
   {
     icon: <Tecs.NestIcon />,
 
     name: 'Nest',
-    experienceTime: '4 months',
   },
   {
     icon: <Tecs.ElixirIcon />,
 
     name: 'Elixir',
-    experienceTime: '1 month',
   },
   {
     icon: <Tecs.PhoenixIcon />,
     name: 'Phoenix',
-    experienceTime: '1 month',
   },
   {
     icon: <Tecs.GraphQLIcon />,
     name: 'GraphQL',
-    experienceTime: '5 month',
   },
   {
     icon: <Tecs.ExpressIcon />,
     name: 'Express',
-    experienceTime: '1.5 year',
   },
   {
     icon: <Tecs.FastifyIcon />,
     name: 'Fastify',
-    experienceTime: '2 months',
   },
   {
     icon: <Tecs.VitestIcon />,
 
     name: 'Vitest',
-    experienceTime: '2 months',
   },
   {
     icon: <Tecs.JestIcon />,
 
     name: 'Jest',
-    experienceTime: '4 months',
   },
   {
     icon: <Tecs.PrismaIcon />,
 
     name: 'Prisma',
-    experienceTime: '9 months',
   },
   {
     icon: <Tecs.DockerIcon />,
 
     name: 'Docker',
-    experienceTime: '1 year',
   },
 ]
 
@@ -91,67 +66,42 @@ export const stylingSkills: ISkill[] = [
     icon: <Tecs.TailwindIcon />,
 
     name: 'TailwindCSS',
-    experienceTime: '1 year',
   },
   {
     icon: <Tecs.SassIcon />,
 
     name: 'Sass',
-    experienceTime: '2 years',
   },
   {
     icon: <Tecs.StyledComponentsIcon />,
     name: 'Styled Components',
-    experienceTime: '2 years',
   },
   {
     icon: <Tecs.CSSIcon />,
 
     name: 'CSS',
-    experienceTime: '2.5 years',
   },
 ]
 
 export const mobileSkills: ISkill[] = [
   {
     icon: <Tecs.ReactIcon />,
-
     name: 'React Native',
-    experienceTime: '5 months',
   },
   {
     icon: <Tecs.ExpoIcon />,
     name: 'Expo',
-    experienceTime: '5 months',
   },
 ]
-
 export const databaseSkills: ISkill[] = [
-  {
-    icon: <Tecs.PostgreIcon />,
-
-    name: 'PostgreSQL',
-    experienceTime: '1 year',
-  },
-  {
-    icon: <Tecs.MongoIcon />,
-
-    name: 'MongoDB',
-    experienceTime: '1.5 year',
-  },
-  {
-    icon: <Tecs.RedisIcon />,
-
-    name: 'Redis',
-    experienceTime: '6 months',
-  },
+  { icon: <Tecs.PostgreIcon />, name: 'PostgreSQL' },
+  { icon: <Tecs.MongoIcon />, name: 'MongoDB' },
+  { icon: <Tecs.RedisIcon />, name: 'Redis' },
 ]
 
 export const cloudSkills: ISkill[] = [
   {
     icon: <Tecs.AwsIcon />,
-
     name: 'AWS',
-    experienceTime: '1 month',
   },
 ]
